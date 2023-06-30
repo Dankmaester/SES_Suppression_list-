c60da24fc431fd7d8c045bc3d2f0103d7c8f248d
Arun Puram <32817609+Dankmaester@users.noreply.github.com>
2023-06-30 10:32:55 +1000

    Update suppression_list_emails.py

--- suppression_list_emails.py
+++ suppression_list_emails.py
@@ -1,60 +1,44 @@
-import os
+import csv
 import boto3
-import argparse
-from datetime import datetime
-import pandas as pd
+import click
+from mypy_boto3_sesv2.client import SESV2Client
 
 
-def setup(profile: str = None):
-    script_dir = os.path.dirname(__file__)
-    rules_file_path = os.path.join(script_dir, "emails.txt")
+def setup(profile: str = None) -> SESV2Client:
+    # Setup AWS client
     session = boto3.Session(profile_name=profile)
-    client = session.client('sesv2')
-    return client, rules_file_path
+    client = session.client("sesv2")
+
+    return client
+
+#getting the emails from SES 
+def get_suppressed_destinations(ses: SESV2Client) -> list[dict]:
+    response = ses.list_suppressed_destinations()
+    destinations = response["SuppressedDestinationSummaries"]
+
+    while response.get("NextToken"):
+        response = ses.list_suppressed_destinations(NextToken=response.get("NextToken"))
+        destinations.extend(response["SuppressedDestinationSummaries"])
+
+    return list(destinations)
+
+#write the emails to a CSV file 
+def write_to_csv(destinations: list[dict]):
+    with open(file="suppressed.csv", mode="w", encoding="utf8", newline="") as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=list(destinations[0].keys()))
+        writer.writeheader()
+        writer.writerows(destinations)
 
 
-def retrieve_suppressed_destinations(client, reasons, start_date, end_date):
-    destinations = []
-    next_token = None
-    while True:
-        params = {
-            'Reasons': reasons,
-            'StartDate': start_date,
-            'EndDate': end_date,
-            'PageSize': 1000,
-        }
-        if next_token:
-            params['NextToken'] = next_token
-
-        response = client.list_suppressed_destinations(**params)
-
-        destinations += response['SuppressedDestinationSummaries']
-        next_token = response.get('NextToken')
-
-        if not next_token:
-            break
-
-    return destinations
-
-
-def main(profile):
-    client, rules_file_path = setup(profile)
-    suppressed_destinations = retrieve_suppressed_destinations(
-        client,
-        reasons=['BOUNCE', 'COMPLAINT'],
-        start_date=datetime(2015, 1, 1),
-        end_date=datetime(2023, 6, 1)
-    )
-
-    df = pd.DataFrame(suppressed_destinations)
-    df.to_csv('suppression_emails_list.csv', index=False)
-    print("CSV file with the emails has been created.")
+@click.command()
+@click.option(
+    "--profile", type=str, default=None, show_default=True, help="Optional AWS profile"
+)
+def main(profile: str):
+    ses = setup(profile)
+    destinations = get_suppressed_destinations(ses)
+    write_to_csv(destinations)
 
 
 if __name__ == "__main__":
-    arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument(
-        "--profile", action="store", type=str
-    )
-    args = arg_parser.parse_args()
-    main(args.profile)
+    main()
